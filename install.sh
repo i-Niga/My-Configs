cf717433aedca013408e4abe0c37b32041487ad7
Ildar Nigamatov <ildarniga@ya.ru>
2022-04-21 02:07:00 +0700

    fix install

--- install.sh
+++ install.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-apt install curl -y
+apt install curl vim vifm tmux zsh  -y
 
-apt install vim -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
 vim -e -s -c "source ~/.vimrc" -c "PlugInstall" -c "qa"
 
-apt install vifm -y
 cp vifm/vifmrc ~/.vifm/vifmrc
 cp vifm/Ansa.vifm ~/.vifm/colors/Ansa.vifm
 
-apt install tmux -y
 cp .tmux.conf ~/.tmux.conf
 
-apt install zsh -y
 chsh -s $(which zsh) 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
